ddb5c3048ca2b9ded5313c86c88e3b309fecd099
Marc Andrés Fontanet <markanft@gmail.com>
2021-11-30 21:04:25 +0100

    update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    maxWidth: {
+      1600: "1600px",
+    },
+    height: {
+      64: "64px",
+      70: "70px",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
